202093b7b75eb92c6ffb7b5130060efa5c5c569a
AmanUpadhyaya10 <amanupadhyaya10@gmail.com>
2025-08-17 16:15:20 +0530

    Add files via upload

---
+++ Task 1-TuteDude.py
@@ -0,0 +1,8 @@
+#Task 1
+x=int(input("Enter first no."))
+y=int(input("Engter second no."))
+print("Addition:",x+y)
+print("Subtraction:",x-y)
+print("Multiplication:",x*y)
+print("Division:",x/y)
+
---
+++ Task 2- Tute Dude.py
@@ -0,0 +1,4 @@
+#Task 2
+f=str(input("Enter first name"))
+l=str(input("Enter last name"))
+print("Hello, ",f+l,"!"," Wlecome to the python program")
